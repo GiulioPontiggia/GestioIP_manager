eb6eda033bccc9a3b6990ce6e6de83fa151a1343
Pontiggia Giulio (EXT) <giulio.pontiggia.ex@prysmiangroup.com>
2024-02-05 18:22:33 +0100

    Used IPaddress library and small changes for error detection

--- gestioip_class.py
+++ gestioip_class.py
@@ -1,59 +1,52 @@
-import json
-import itertools
+import ipaddress
 import platform
 import os
-import sys
 import requests
 import time
 
 from dotenv import load_dotenv
 from prettytable import PrettyTable
 
+# Gets credentials from .env file encrypted
 load_dotenv()
 cred = os.getenv("KEY")
 cred = 'Basic ' + cred
 
-def match_sm_ip(ip, sm):
-    net = ''
-    for (x, y) in zip(ip.split('.'), sm.split('.')):
-        x = int(x)
-        y = int(y)
-        net += str(x & y)
-        net += '.'
-    return(net[:-1])
 
-
+# Clear screen
 def clear_screen():
     op_sys = platform.platform()
     if "Windows" in op_sys:
         os.system("cls")
     else:
         os.system("clear")
+
 class GestioIP():
 
     def __init__(self):
         self.choice = ''
 
-    def get_net(self, ip):
+    # Function to get a network information through API
+    def get_net(self, ip, single_request=True):
+        # Initialization of the table
         net_table = PrettyTable()
         net_table.align = 'l'
         headers= {
         'Authorization' : cred
         }
-
         query_params={
                     'request_type' : 'readNetwork',
                     'output_type' : 'json',
                     'client_name' : 'DEFAULT',
                     'ip' : ip
                 }
-
         url="http://10.123.86.151/gestioip/api/api.cgi"
         try:
             response = requests.get(url, headers=headers, params=query_params, verify=False)
             response = response.json()
             response = response["readNetworkResult"]["Network"]
             net_table.field_names = ["Field", "Value"]
+            # Add each entry in the table
             for entry in response:
                 if response[entry]:
                     if type(response[entry]) == dict:
@@ -62,10 +55,13 @@ class GestioIP():
                                 net_table.add_row([entr, response[entry][entr]])
                     else:
                         net_table.add_row([entry, response[entry]])
+            # Return both the response as dictionary and the table formatted
             return([response, net_table])
         except:
-            None
-        
+            if single_request:
+                print(f"[*] Network {ip} not found")
+    
+    # Get host information through API
     def get_host(self, ip):
         clear_screen()
         host_table = PrettyTable()
@@ -99,6 +95,59 @@ class GestioIP():
         except:
             print(f"Host not found, check the IP address ({ip})...")
     
+    def list_networks(self, site):
+        net_table = PrettyTable()
+        net_table.align = 'l'
+
+        headers= {
+        'Authorization' : cred
+        }
+        # Filter the networks based on the site name
+        query_params={
+                    'request_type' : 'listNetworks',
+                    'client_name' : 'DEFAULT',
+                    'output_type' : 'json',
+                    'filter' : 'site::' + site,
+                }
+
+        url="http://10.123.86.151/gestioip/api/api.cgi"
+        try:
+            response = requests.get(url, headers=headers, params=query_params, verify=False)
+            response = response.json()['listNetworksResult']['NetworkList']['Network']
+            networks = []
+            net_table.field_names = ["Network", "Site", "Category","Comment", "VLAN"]
+            for entry in response:
+                net = entry.split(',')
+                add_row = []
+                for i in net:
+                    if i:
+                        add_row.append(i)
+                net_table.add_row(net)
+                networks.append(add_row)
+            
+            return([networks, net_table])
+        except:
+            print(f"Site not found check for errors {site}")
+            return None          
+        
+    # Change the network with a new site
+    def update_network(self, net_ip, new_site):
+
+        headers= {
+        'Authorization' : cred
+        }
+        query_params={
+                    'request_type' : 'updateNetwork',
+                    'client_name' : 'DEFAULT',
+                    'ip' : net_ip,
+                    'new_site' : new_site
+                }
+
+        url="http://10.123.86.151/gestioip/api/api.cgi"
+        response = requests.get(url, headers=headers, params=query_params, verify=False)
+        print(response)
+    
+    # First menu visualised at the beginning of the execution
     def quick_menu(self):
         while True:
             clear_screen()
@@ -118,84 +167,39 @@ class GestioIP():
                 '''
             print(menu)
             self.choice = input("Insert choice: ")
+
             if self.choice.lower().strip() == 'm':
                 self.menu()
+
             elif self.choice.lower().strip() == 'q':
                 break
+
             else:
                 self.get_host(self.choice)
+                
+                # Iterate from subnet mask /10 to /31
                 net_requested = []
-                for i in range(0, 17):
-                    if i <= 8:
-                        m = '255.255.255.' + str(2**i - 1)
-                    else:
-                        m = ('255.255.' + str(2**(8-(i-8)) -1) +  '.0')
-
-                    network = match_sm_ip(self.choice, m)
-                    
+                for i in range(16, 31):
+                    sm = ipaddress.IPv4Network(f"0.0.0.0/{i}", strict=False).netmask        # Finds sub mask in format x.x.x.x
+                    network = ipaddress.IPv4Network(f"{self.choice}/{sm}", strict=False).network_address    # Finds the network based on ip inserted and sm
+                    # For any network recall the get_network function to find network information
                     try:
+                        # Request the network
                         if network not in net_requested:
+                            resp = self.get_net(network, False)
                             net_requested.append(network)
-                            resp = self.get_net(network)
-                            data = resp[0]
-                            table = resp[1]
-                            if int(data['BM']) <= 24:
-                                sub_mas = '255.255.' + str(2**(int(data['BM'])-16)-1) + '.0'
-                            else:
-                                sub_mas = '255.255.255.' + str(2**(int(data['BM'])-24)-1)
-                            if match_sm_ip(self.choice, sub_mas) == data['IP']:
-                                print(table)
+                            net = resp[0]['IP']
+                            mask = resp[0]['BM'] 
+                            # Check if the host ip is in the network requested
+                            net_obj = ipaddress.IPv4Network(f"{net}/{mask}", strict=False)
+                            ip_obj = ipaddress.IPv4Address(self.choice)
+                            if ip_obj in net_obj:
+                                print(resp[1])
+
                     except:
                         continue
+
                 input("Press ENTER to continue...")
-
-    def list_networks(self, site):
-        net_table = PrettyTable()
-        net_table.align = 'l'
-
-        headers= {
-        'Authorization' : cred
-        }
-
-        query_params={
-                    'request_type' : 'listNetworks',
-                    'client_name' : 'DEFAULT',
-                    'output_type' : 'json',
-                    'filter' : 'site::' + site,
-                }
-
-        url="http://10.123.86.151/gestioip/api/api.cgi"
-        response = requests.get(url, headers=headers, params=query_params, verify=False)
-        response = response.json()['listNetworksResult']['NetworkList']['Network']
-        networks = []
-        net_table.field_names = ["Network", "Site", "Category","Comment", "VLAN"]
-        for entry in response:
-            net = entry.split(',')
-            add_row = []
-            for i in net:
-                if i:
-                    add_row.append(i)
-            net_table.add_row(net)
-            networks.append(add_row)
-        
-        return([networks, net_table])
-        
-    def update_network(self, net_ip, new_site):
-
-        headers= {
-        'Authorization' : cred
-        }
-
-        query_params={
-                    'request_type' : 'updateNetwork',
-                    'client_name' : 'DEFAULT',
-                    'ip' : net_ip,
-                    'new_site' : new_site
-                }
-
-        url="http://10.123.86.151/gestioip/api/api.cgi"
-        response = requests.get(url, headers=headers, params=query_params, verify=False)
-        print(response)
 
     def menu(self):
         
@@ -216,18 +220,22 @@ class GestioIP():
                 '''
             print(menu)
             self.choice = input("\n[ ] choice: ")
+
             if self.choice == '1':
                 clear_screen()
                 ip = input("Network IP [x.x.x.x]: ")
-                self.get_net(ip)
+                print(self.get_net(ip)[1])
                 input("Press ENTER to continue...")
+
             elif self.choice == '2':
                 clear_screen()
                 net_ip = input("Network IP [x.x.x.x] or site name: ")
                 new_site = input("New site: ")
+                # Check if the user inserted a site or a network
                 if len(net_ip.split('.')) == 4:
                     self.update_network(net_ip, new_site)
                 else:
+                    # Goes through all the networks of the site and print the table
                     net_list = self.list_networks(net_ip)[0]
                     for net in net_list:
                         network = net[0]
@@ -239,16 +247,16 @@ class GestioIP():
             elif self.choice == '3':
                 clear_screen()
                 site = input('Site: ')
-                print(self.list_networks(site)[1])
+                net_list = self.list_networks(site)
+                if net_list:
+                    print(net_list[1])
                 input("Press ENTER to continue...")
-            elif self.choice == '4':
-                clear_screen()
-                print("WIP")
+
             elif self.choice.lower().strip() == 'q':
                 print("Exiting...")
                 break
             else:
-                print("Input Error...")
+                continue
  
 if __name__ == '__main__':
     Gestio = GestioIP()
